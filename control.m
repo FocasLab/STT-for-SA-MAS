5ebbe0a0f3d7b5406f385d2c0b2904105880ab8c
Siddhartha Upadhyay <130141078+UpadhyaySid@users.noreply.github.com>
2026-04-08 17:45:57 +0530

    Refactor control function for improved calculation

--- control.m
+++ control.m
@@ -1,13 +1,7 @@
 function u = control(tube,rad,robot)
-% u = 10*(tube-robot);
-
-a = 7;
-
 e = norm(robot - tube)/rad;
-ub = 20;
-%u = -ub*tanh(a*e).*(1-exp(-a*a*e.^2)) * (robot - tube) / norm(robot - tube);
-u = -ub * (robot - tube) * log((1+e)/(1-e));
+k = 20;
+u = -k * (robot - tube) * log((1+e)/(1-e));
 u = real(u);
-
 end
 
